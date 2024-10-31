253324e4f5ac67cc460f4db75dd1889810ea3b16
Loc Le <lpl212757@gmail.com>
2024-10-31 18:18:16 +0700

    My default layout of new session

--- templates/session.sh
+++ templates/session.sh
@@ -1,19 +1,23 @@
 # Set a custom session root path. Default is `$HOME`.
 # Must be called before `initialize_session`.
-#session_root "~/Projects/{{SESSION_NAME}}"
+session_root "~/Coding/{{SESSION_NAME}}"
 
 # Create session with specified name if it does not already exist. If no
 # argument is given, session name will be based on layout file name.
 if initialize_session "{{SESSION_NAME}}"; then
 
   # Create a new window inline within session layout definition.
-  #new_window "misc"
+  new_window "Editor"
+  new_window "Fish"
+  # Split window into panes.
+    split_v 50
+    run_cmd "htop"
 
   # Load a defined window layout.
   #load_window "example"
 
   # Select the default active window on session creation.
-  #select_window 1
+  select_window 0
 
 fi
 
